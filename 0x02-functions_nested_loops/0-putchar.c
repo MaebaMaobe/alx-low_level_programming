3859dfa99535299a70d7cce9e94ff9ce42599cd0
MaebaMaobe <maebamaobe.com>
2022-03-15 15:27:44 +0000

    printing putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints Holberton
+ *
+ * Description: prints Holberton
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(text[i]);
+	}
+	putchar(10);
+
+	return (0);
+}
