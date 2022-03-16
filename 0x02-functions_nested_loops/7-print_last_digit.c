254b9ff1c954466d50b70f12c15c25f74d16ac34
MaebaMaobe <maebamaobe.com>
2022-03-16 13:09:58 +0000

    Digits

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,25 +1,17 @@
 #include "main.h"
-
 /**
  * print_last_digit - prints last digit
- *
- * @i: digit to be tested
- *
- * Return: Value of the digit.
- */
-int print_last_digit(int i)
-{
-	int m;
+ * @num: number to compute last digit for
+ * Return: last digit
+ **/
 
-	m = i % 10;
-	if (m < 0)
-	{
-		_putchar(-m + 48);
-		return (-m);
-	}
-	else
-	{
-		_putchar(m + 48);
-		return (m);
-	}
+
+int print_last_digit(int num)
+{
+	num = num % 10;
+
+	if (num < 1)
+		num = -num;
+	_putchar(num + '0');
+	return (num);
 }
